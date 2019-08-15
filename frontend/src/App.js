0c75afb86bb402ff879a1baccc0c87f18b2c37d0
Chinseok Lee <me@askcompany.kr>
2019-08-15 11:10:35 +0900

    react-router-dom 예시 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
+import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
 import {Icon, Menu, Layout} from "antd";
 import "./App.css";
 import logo from "./assets/logo_w.png";
 import Planner from "./components/planner";
+import Home from "./components/home";
 
 const {Footer, Sider} = Layout;
 const {SubMenu} = Menu;
@@ -18,50 +20,57 @@ const App = () => {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
-        <div>
-          <img src={logo} style={{ padding: '10px' }} alt="logo" />
-        </div>
-        <Menu theme="dark" defaultSelectedKeys={['menu-planner']} mode="inline">
-          <Menu.Item key="menu-planner">
-            <Icon type="table" />
-            <span>교수 계획표</span>
-          </Menu.Item>
-          <Menu.Item key="menu-2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <SubMenu
-            key="submenu-1"
-            title={
-              <span>
-                  <Icon type="user" />
-                  <span>User</span>
-                </span>
-            }
-          >
-            <Menu.Item key="menu-3">Tom</Menu.Item>
-            <Menu.Item key="menu-4">Bill</Menu.Item>
-            <Menu.Item key="menu-5">Alex</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Planner />
-        {/*<Header style={{ background: '#fff', padding: 0 }} />*/}
-        {/*<Content style={{ margin: '0 16px' }}>*/}
-        {/*  <Breadcrumb style={{ margin: '16px 0' }}>*/}
-        {/*    <Breadcrumb.Item>User</Breadcrumb.Item>*/}
-        {/*    <Breadcrumb.Item>Bill</Breadcrumb.Item>*/}
-        {/*  </Breadcrumb>*/}
-        {/*  <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>*/}
-        {/*    Bill is a cat.*/}
-        {/*  </div>*/}
-        {/*</Content>*/}
-        <Footer style={{ textAlign: 'center' }}>
-          부산대학교 ©2019. All rights reserved.
-        </Footer>
-      </Layout>
+      <Router>
+        <Sider collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
+          <div>
+            <NavLink to="/">
+              <img src={logo} style={{ padding: '10px' }} alt="logo" />
+            </NavLink>
+          </div>
+          <Menu theme="dark" defaultSelectedKeys={['menu-planner']} mode="inline">
+            <Menu.Item key="menu-planner">
+              <NavLink to="/planner">
+                <Icon type="table" />
+                <span className="nav-text">교수 계획표</span>
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key="menu-2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="submenu-1"
+              title={
+                <span>
+                    <Icon type="user" />
+                    <span>User</span>
+                  </span>
+              }
+            >
+              <Menu.Item key="menu-3">Tom</Menu.Item>
+              <Menu.Item key="menu-4">Bill</Menu.Item>
+              <Menu.Item key="menu-5">Alex</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Route path="/" exact component={Home} />
+          <Route path="/planner" component={Planner} />
+          {/*<Header style={{ background: '#fff', padding: 0 }} />*/}
+          {/*<Content style={{ margin: '0 16px' }}>*/}
+          {/*  <Breadcrumb style={{ margin: '16px 0' }}>*/}
+          {/*    <Breadcrumb.Item>User</Breadcrumb.Item>*/}
+          {/*    <Breadcrumb.Item>Bill</Breadcrumb.Item>*/}
+          {/*  </Breadcrumb>*/}
+          {/*  <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>*/}
+          {/*    Bill is a cat.*/}
+          {/*  </div>*/}
+          {/*</Content>*/}
+          <Footer style={{ textAlign: 'center' }}>
+            부산대학교 ©2019. All rights reserved.
+          </Footer>
+        </Layout>
+      </Router>
     </Layout>
 
   );
---
+++ frontend/src/components/home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Layout, PageHeader} from "antd";
+import queryString from "query-string";
+const {Content} = Layout;
+
+
+const Home = ({history, location, match}) => {
+  // const qs = queryString.parse(location.search);
+  return (
+    <>
+      <PageHeader title={"Home"} />
+
+      <Content>
+        Home Component
+      </Content>
+    </>
+  );
+};
+
+
+export default Home;
